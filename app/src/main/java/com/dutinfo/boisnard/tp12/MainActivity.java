5b8c39828ba512a2ce2f8e34dd15e47531a09bb6
kernoeb <kernoeb@protonmail.com>
2020-02-05 22:42:34 +0100

    Implement duration time dialog :tada:

--- app/src/main/java/com/dutinfo/boisnard/tp12/MainActivity.java
+++ app/src/main/java/com/dutinfo/boisnard/tp12/MainActivity.java
@@ -69,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements AdapterList.Recyc
 
         this.layoutManager = new LinearLayoutManager(getApplicationContext());
 
-
         this.recyclerView.setLayoutManager(layoutManager);
         this.recyclerView.setItemAnimator(new DefaultItemAnimator());
 
